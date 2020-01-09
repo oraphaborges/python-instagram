015e246f728783b3096bcda6b1591891d41673ec
Borges <oborgesrapha@gmail.com>
2020-01-09 11:32:30 -0300

    conclude image download by hash tag

--- instagram.py
+++ instagram.py
@@ -1,8 +1,10 @@
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup as bs
 from decouple import config
 from time import sleep
+import requests
 
 # LOADING BROWSER DRIVE
 webdriver = webdriver.Chrome('drivers/chromedriver')
@@ -24,6 +26,58 @@ sleep(3)
 tags = ['lasanha','guioza','pizza']
 for tag in tags:
     webdriver.get(f'https://www.instagram.com/explore/tags/{tag}/')
-    sleep(2)
 
+    sleep(3)
+
+    MAX_SCROLL = 2
+    SCROLL = 0
+    images_unique=[]
+    # Get scroll height
+    last_height = webdriver.execute_script("return document.body.scrollHeight")
+    while True:
+        # Scroll down to bottom
+        webdriver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+         # Wait to load page
+        sleep(2)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = webdriver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            webdriver.execute_script("window.scrollTo(document.body.scrollHeight,0);")
+            break
+
+        # This means that there is still photos to scrap
+        if SCROLL < MAX_SCROLL:
+            last_height = new_height 
+        else:
+            break
+        SCROLL += 1
+        sleep(2)
+
+        # Retrive the html
+        html_to_parse=str(webdriver.page_source)
+        html=bs(html_to_parse,"html5lib")
+
+        # Get the image's url
+        images_url=html.findAll("img", {"class": "FFVAD"})
+
+        # Check if they are unique
+        in_first = set(images_unique)
+        in_second = set(images_url)
+
+        in_second_but_not_in_first = in_second - in_first
+
+        result = images_unique + list(in_second_but_not_in_first)
+        images_unique=result
+
+    for i in range(len(images_unique)):
+    # for i in range(5): # for teste
+        # Save each image.jpg file
+        name=f"output/{tag}_{i}.jpg"
+        with open(name, 'wb') as handler:
+            img_data = requests.get(images_unique[i].get("src")).content
+            handler.write(img_data)
+
+#Close the webdriver   
 webdriver.close()
